8b1fdd75aa3c623d2700f52d12dd817b52693789
dennis-phillips <dennis_phillips@outlook.com>
2020-08-05 12:14:11 -0400

    another commit

---
+++ coding-events-api/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coding_events_api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
